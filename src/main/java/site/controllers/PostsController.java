66743ff07f95f9451536e125dfff705402bc8225
Denis_Lapshin <denislapshin1@gmail.com>
2021-11-08 15:31:26 +0200

    delete comments in PostController

--- src/main/java/site/controllers/PostsController.java
+++ src/main/java/site/controllers/PostsController.java
@@ -10,10 +10,6 @@ public class PostsController
     @GetMapping("/posts")
     public String showPosts(Model model)
     {
-        // вытащит заголовоки и даты из базы данных
-        
-        // засунуть заголовки и даты в модель для представления в виде объекта Iterable
-        
         return "posts";
     }
 }
