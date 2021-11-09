6a0a766c6bc069fa127d9aa1e68571262a80a78a
Denis_Lapshin <denislapshin1@gmail.com>
2021-11-09 14:48:30 +0200

    created /posts page

--- src/main/java/site/controllers/PostsController.java
+++ src/main/java/site/controllers/PostsController.java
@@ -3,6 +3,10 @@ package site.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import site.model.Post;
+import site.model.PostDao;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostsController
@@ -10,6 +14,20 @@ public class PostsController
     @GetMapping("/posts")
     public String showPosts(Model model)
     {
+        // truncate text to 150 characters and add " ..."
+        ArrayList<Post> posts = PostDao.getAllPosts();
+        for (int i = 0; i < posts.size(); i++)
+        {
+            String text = posts.get(i).getText();
+            if (text.length() > 150)
+            {
+                text = text.substring(0, 151);
+                text = text.trim().concat(" ...");
+            }
+            posts.get(i).setText(text);
+        }
+        
+        model.addAttribute("posts", posts);
         return "posts";
     }
 }
