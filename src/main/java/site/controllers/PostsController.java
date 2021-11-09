00ee4fa625769472878a18d938f98a067bdab2d1
Denis_Lapshin <denislapshin1@gmail.com>
2021-11-09 23:30:42 +0200

    added full display of each news post

--- src/main/java/site/controllers/PostsController.java
+++ src/main/java/site/controllers/PostsController.java
@@ -3,6 +3,7 @@ package site.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import site.model.Post;
 import site.model.PostDao;
 
@@ -16,18 +17,25 @@ public class PostsController
     {
         // truncate text to 150 characters and add " ..."
         ArrayList<Post> posts = PostDao.getAllPosts();
-        for (int i = 0; i < posts.size(); i++)
+        for (Post post : posts)
         {
-            String text = posts.get(i).getText();
+            String text = post.getText();
             if (text.length() > 150)
             {
                 text = text.substring(0, 151);
                 text = text.trim().concat(" ...");
             }
-            posts.get(i).setText(text);
+            post.setText(text);
         }
         
         model.addAttribute("posts", posts);
         return "posts";
+    }
+    
+    @GetMapping("/posts/{id}")
+    public String showPostById(Model model, @PathVariable("id") int id)
+    {
+        model.addAttribute("post", PostDao.getAllPosts().get(id - 1));
+        return "post";
     }
 }
