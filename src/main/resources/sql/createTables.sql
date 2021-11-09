e60b225ad40b761eea0d6d10383acc7746123111
Denis_Lapshin <denislapshin1@gmail.com>
2021-11-09 14:44:03 +0200

    update sql scripts

--- src/main/resources/sql/createTables.sql
+++ src/main/resources/sql/createTables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE news (
     id SERIAL UNIQUE PRIMARY KEY,
-    header VARCHAR(50) NOT NULL,
+    header VARCHAR(150) NOT NULL,
     image_filename VARCHAR(15),
     text VARCHAR(5000) NOT NULL,
     creation_date DATE NOT NULL
--- src/main/resources/sql/insertData.sql
+++ src/main/resources/sql/insertData.sql
@@ -1,27 +1,46 @@
 INSERT INTO news
 VALUES (DEFAULT,
-        'Заголовок1',
-        'Текст новости1',
-        'img1.jpg',
+        'The US is reopening its borders to Europe, the global epicenter of Covid-19',
+        'img1.png',
+        'Paris (CNN)The United States has reopened its borders to vaccinated international travelers, ending a 20-month travel ban at the same moment Europe is battling a surge of Covid-19 cases that has pushed the continent back into the epicenter of the pandemic.
+         Fully vaccinated travelers from 33 countries -- including the United Kingdom and much of Europe -- can now enter the US without needing to quarantine, provided they have proof of vaccination and a negative viral test.
+         Families and friends separated since the start of the pandemic streamed into airports across major European cities on Monday morning, excited to see loved ones for the first time since former President Donald Trump imposed tough travel restrictions at the outset of the pandemic in an effort to control the virus.
+         But beyond these happy scenes is a darker backdrop.
+         Europe is in the grip of a fresh wave of coronavirus cases, with the World Health Organization last week warning the region is once again "at the epicenter" of the pandemic.
+         Large swathes of Europe are battling to beat back surges of the Delta variant, amid the relaxation of restrictions and stuttering vaccine rollouts in some countries, with the WHO warning half a million Europeans could die with Covid-19 in a potentially devastating winter.
+         Germany on Monday recorded its highest seven-day incidence rate since the pandemic began. The same day, neighboring Austria banned unvaccinated people from restaurants and hotels amid a surge in cases. Iceland has also reintroduced masks and social-distancing rules following a rise. And cases are hovering at record levels in Russia, Ukraine and Greece.',
         '2001-01-01'),
        (DEFAULT,
-        'Заголовок2',
-        'Текст новости2',
-        'img3.jpg',
+        'AMD continues to put pressure on Intel by nearing 25% CPU market share',
+        'img2.png',
+        'AMD''s phoenix-like rise from the ashes isn''t thanks to some miracle, but rather years of hard work to cement itself as serious competition against rival Intel in the CPU market once again. It seems like that''s starting to pay off, with Hardware Times reporting on the latest Mercury Research results, finding that Team Red has gained revenue share for the sixth straight quarter.
+        Not only that, but this increase also resulted in the chipmaker hitting a milestone for x86 CPU market share, currently holding 24.6% in Q3 2021. This is the second-highest x86 share for AMD ever, just short of its highest share of 25.3% dating back to Q4 2006.
+        On top of its desktop success, AMD notebook x86 unit share (excluding IoT, or Internet of Things) was 22%, an all-time high for the company.
+        For the unaware, x86 is essentially the language that a CPU speaks, referring to a family of instruction set architectures based on the Intel 8086 that was released back in 1978. Most modern desktop CPUs are x86, and AMD Ryzen and EPYC processors have gained increased popularity over Intel alternatives in recent years thanks to their relatively affordable price compared to performance, which has helped increase the company''s public opinion.
+        AMD’s Ryzen CPUs have been scarce over the last few months after a rapid rise in popularity and lack of available inventory. This was in part due to the ongoing silicon shortage, something that experts anticipate could be an issue into 2023, but these results prove that AMD has succeeded against all odds to claw some market share back from Intel even in a difficult market.',
         '2002-02-02'),
        (DEFAULT,
-        'Заголовок3',
-        'Текст новости3',
-        'img3.jpg',
+        'Samsung Galaxy S22 rumors: February release date, leaked photos and more',
+        'img3.png',
+        'We still have some time until Samsung takes the wraps off its latest flagship lineup, but rumors about the new phones continue to buzz in the meantime. Following the allegedly leaked pictures of the Galaxy S22, the latest gossip from leaker Jon Prosser says Samsung''s new smartphone will be revealed at an Unpacked event in February. Samsung''s August event excited us with two new foldable phones, the Galaxy Z Fold 3 and Z Flip 3, alongside the company''s latest smartwatch, the Galaxy Watch 4, and the Galaxy Buds 2. Then in October, Samsung showed off more options to customize its foldable phones. But we still don''t know exactly when Samsung will take the wraps off the rumored Galaxy S22. This year''s Galaxy S21 lineup was unveiled in January, which could mean we''ll see Samsung''s new flagship early next year. The Samsung Galaxy S21 impressed us with its solid all-round performance and better balance of features and price. And the Galaxy S21 Ultra blew us away with its incredible camera setup and its ability to take superb zoomed-in shots. But what about the follow-up series? We''re curious to see what the Galaxy S22 might bring, so here''s what we think.',
         '2003-03-03'),
        (DEFAULT,
-        'Заголовок4',
-        'Текст новости4',
-        'img4.jpg',
+        'George W. Bush shares wedding photo to honor anniversary with wife Laura',
+        'img4.png',
+        'A day after celebrating his wife''s birthday, George W. Bush is cherishing their 44th wedding anniversary.
+         The 43rd president marked the occasion on Friday by sharing a photo on Instagram of him and former first lady Laura Bush cutting the cake at their wedding back in 1977. "November is a special month in our house. Not only did we get to celebrate my beautiful wife’s birthday yesterday, but also the great gift of marriage today. Happy Anniversary @laurawbush!"
+         The couple got married three months after they first met at a friend''s barbecue in Texas and went on a date the next day to play miniature golf.
+            “I kind of think it was love at first sight,” Laura Bush said on TODAY in 2018.',
         '2004-04-04'),
        (DEFAULT,
-        'Заголовок5',
-        'Текст новости5',
-        'img5.jpg',
+        'Rush-hour delays after accident on State Highway 2, Wellington',
+        'img5.png',
+        'Rush-hour traffic has backed up following a crash on State Highway 2 between Ngauranga and Petone on Monday afternoon.
+         Police were notified of the accident about 4.30 pm, a police spokesperson said.
+         A car collided with a barrier, she said.
+         Four people were in the car, and two people were initially trapped, she said, though there are no immediate reports of injuries.
+         Fire and Emergency shift manager Murray Dunbar said two fire trucks attended the scene and helped get two people out of the vehicle. They are now out and the fire appliances have left the scene.
+         Traffic has backed up on the highway. It takes a village to produce a newspaper like The Dominion Post and keep the Wellington section of Stuff so fresh and fancy.
+         Our dedicated team works 24/7 to investigate the Capital''s housing crisis and crumbling infrastructure, to cover its vibrant arts sector, to review local food ventures, and report on the world-class start-up scene. That work costs more than our time and energy. And that''s why we''re asking you to support us with more than your attention.
+         If our news reports, reviews, live blogs and videos from Wellington have an impact on you, please make a contribution today.',
         '2005-05-05');
-
