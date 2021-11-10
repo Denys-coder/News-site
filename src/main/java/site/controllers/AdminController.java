3f5114d0c936e9c90402c21dbc19d29b10dbe7c2
Denis_Lapshin <denislapshin1@gmail.com>
2021-11-10 17:13:01 +0200

    fixed some issues

--- src/main/java/site/controllers/AdminController.java
+++ src/main/java/site/controllers/AdminController.java
@@ -2,12 +2,16 @@ package site.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class AdminController
 {
     @GetMapping("/admin")
-    protected String adminPage()
+    protected String admin()
     {
         return "admin";
     }
