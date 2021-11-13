b69ba8c8ff19a23d6f20349e851d062b951e6ebd
Denis_Lapshin <denislapshin1@gmail.com>
2021-11-14 01:41:59 +0200

    moved main configuration file

--- src/main/java/site/NewsSiteApplication.java
+++ src/main/java/site/NewsSiteApplication.java
@@ -1,13 +1,12 @@
-package site.config;
+package site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan("site")
+@SpringBootApplication
 public class NewsSiteApplication
 {
     public static void main(String[] args)
