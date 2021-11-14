9f7a9c8d2e9fac286a60eeaf83ab28b70f94520b
Denis_Lapshin <denislapshin1@gmail.com>
2021-11-14 02:28:32 +0200

    moved main Spring Boot configuration file in config package

--- src/main/java/site/config/NewsSiteApplication.java
+++ src/main/java/site/config/NewsSiteApplication.java
@@ -1,12 +1,13 @@
-package site;
+package site.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("site")
+@Configuration
 public class NewsSiteApplication
 {
     public static void main(String[] args)
